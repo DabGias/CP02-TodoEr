ff2835d1f21a3228df169ab6703dca5882ef7a88
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-26 10:14:00 -0300

    coommit flatlist

--- src/screens/Main.jsx
+++ src/screens/Main.jsx
@@ -1,12 +1,43 @@
-import { View, Text } from "react-native"
-import { getUser } from "../../firebaseConfig"
+import { useEffect, useState } from "react"
+import { FlatList, Image, View, Text, Pressable, SafeAreaView } from "react-native"
 
-export function Main() {
-    const user = getUser()
-    
+function ProdtCard({ id, title, description, image, price }) {
     return(
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text>Você está logado como: {user["email"]}</Text>
+        <View>
+            <Image source={{ uri: image }}/>
+            <Text>{title}</Text>
+            <Text>{description}</Text>
+            <Text>{price}</Text>
         </View>
+    )
+}
+
+export function Main({ navigation }) {
+    const [prodts, setProdts] = useState([])
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    async function getData() {
+        const resp = await fetch("https://fakestoreapi.com/products")
+
+        setProdts(await resp.json())
+    }
+
+    return(
+        <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <FlatList
+                
+            />
+
+            <View>
+                <Pressable
+                    onPress={() => { navigation.navigate('Favs') }}
+                >
+                    <Text>Ver meus favoritos</Text>
+                </Pressable>
+            </View>
+        </SafeAreaView>
     )
 }
